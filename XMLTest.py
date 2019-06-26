3b75bd3b99d553d7228bea5a50422ce6bafc6942
simonjholmes <simon.holmes@adventone.com>
2019-06-26 14:36:34 +1000

    More

--- XMLTest.py
+++ XMLTest.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
-def main():
-  print("Hello World!")
-  
-main()
-print("Guru99")
+import threading
+
+innerArray = [1,2,3]
+outerArray = [innerArray, 4, 5]
+
+for element in outerArray:
+  print("{}, ".format(element), end = "")
+
+print("".format(element))
+
+for element in outerArray:
+
+  if isinstance(element, list):
+
+    for subElement in element:
+        print("subElement {}, ".format(subElement), end = "")
+
+  else:
+    print("element {}, ".format(element), end = "")
+
+print("".format(element))
--- functions/DriveForXRotations.py
+++ functions/DriveForXRotations.py
@@ -17,13 +17,13 @@ import constants
 
 def driveForXRotations(debug, stop, rotations, speed):
 
+    if debug & constants.DEBUG and debug & constants.DEBUG_THREAD_LIFECYCLE:
+        print("Start driveForXRotations({}, {}), thread {}".format(rotations, speed, threading.current_thread().ident), file=stderr)
+
     motorLeft = LargeMotor(constants.OUTPUT_LARGE_MOTOR_LEFT)
     tank_pair = MoveTank(constants.OUTPUT_LARGE_MOTOR_LEFT, constants.OUTPUT_LARGE_MOTOR_RIGHT)
 
     rotationB = motorLeft.position
-
-    if debug & constants.DEBUG and debug & constants.DEBUG_THREAD_LIFECYCLE:
-        print("Start driveForXRotations({}, {}), thread {}".format(rotations, speed, threading.current_thread().ident), file=stderr)
 
     if debug & constants.DEBUG and debug & constants.DEBUG_MOVEMENT_ROTATION_STARTING_POSITION_POSITION:
         print("> Starting position {}". format(rotationB), file = stderr)
@@ -42,13 +42,14 @@ def driveForXRotations(debug, stop, rotations, speed):
             if debug & constants.DEBUG and debug & constants.DEBUG_THREAD_LIFECYCLE:
                 print("Kill driveForXRotations({}, {}), thread {}.".format(rotations, speed, threading.current_thread().ident), file=stderr)
             break
+            
+    tank_pair.off()
+
+    if debug & constants.DEBUG and debug & constants.DEBUG_MOVEMENT_ROTATION_FINAL_POSITION:
+        print("> Final position {}". format(motorLeft.position), file = stderr)
 
     if not stop():
 
-        if debug & constants.DEBUG and debug & constants.DEBUG_MOVEMENT_ROTATION_FINAL_POSITION:
-            print("> Final position {}". format(rotationB), file = stderr)
-            
-        tank_pair.off()
-
         if debug & constants.DEBUG and debug & constants.DEBUG_THREAD_LIFECYCLE:
             print("End driveForXRotations({}, {}), thread {}.".format(rotations, speed, threading.current_thread().ident), file=stderr)
+
